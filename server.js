b0f2378dcb652b1c54538273e6126dcbc36c8688
Denis S Dujota <denisdujota@gmail.com>
2025-09-03 19:38:36 +0300

    add mongoose lib

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // Here is where we import modules
 // We begin by loading Express
 const express = require('express');
+const morgan = require('morgan');
 
 const PORT = 3000;
 const app = express();
+// Middleware
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 app.listen(PORT, () => {
   console.log('Listening on port 3000');
