ed908fc324ad426a0be71cd22b45efe9780a9f14
Denis S Dujota <denisdujota@gmail.com>
2025-09-03 20:54:11 +0300

    finish create fruit

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
-// Here is where we import modules
-// We begin by loading Express
+/* eslint-disable no-console */
+const dotenv = require('dotenv');
+
+dotenv.config();
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const PORT = 3000;
 const app = express();
+
+// DB Connection
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Models
+const Fruit = require('./models/fruit');
+
 // Middleware
+
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.get('/fruits/new', (req, res) => {
+  res.render('fruits/new.ejs');
+});
+
+app.post('/fruits', async (req, res) => {
+  console.log(req.body);
+  if (req.body.isReadyToEat === 'on') {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  await Fruit.create(req.body);
+
+  res.redirect('/fruits/new');
 });
 
 app.listen(PORT, () => {
